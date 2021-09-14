7178c3ceaedb77a671ab8a7e39d857e6ae886172
jose-salguero <jsalgueroc@miumg.edu.gt>
2021-09-13 20:55:24 -0600

    Agregar archivos de proyecto.

---
+++ CRUD_NET/ConexionBD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_NET
+{
+    public class ConexionBD
+    {
+        private string contenido = "server=localhost;database=db_empresa_2021;user=root;password=admin1234";
+        public MySqlConnection conectar = new MySqlConnection();
+        public MySqlDataAdapter adaptador = new MySqlDataAdapter();
+        public void AbrirConexion()
+        {
+            try
+            {
+                string sConn;
+                sConn = contenido;
+                conectar = new MySqlConnection();
+                conectar.ConnectionString = sConn;
+                conectar.Open();
+                System.Diagnostics.Debug.WriteLine("Conexion Exitosa");
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Conexion Erronea");
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            if (conectar.State == ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+        }
+    }
+}
